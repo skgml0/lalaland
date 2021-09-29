8265eaf652dbfeadeb024d352bb4ab0548215bbc
황나희 <hnh2714@gmail.com>
2021-09-29 21:55:40 +0900

    0929(3)

---
+++ 기초문제/10808알파벳개수.py
@@ -0,0 +1,5 @@
+# 알파벳 직접 적는 방법 # 다른 방법.
+alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+arr= input()
+for a in alphabet:
+    print(arr.count(a),end=" ")
